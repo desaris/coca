567da49a9567131a527d1727ab3aff4cd5b9812a
Des <dolboebi1@yandex.ru>
2025-11-18 22:58:46 +0700

    Added insight section

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,11 @@ import '/src/public/scss/main.scss';
 
 import { useTheme } from './components/theme';
 import { useBurger } from './components/burger';
+import {
+    useInsightSlider,
+    useTestimonialsSlider,
+} from './components/home/slider.js';
 useTheme();
 useBurger();
+useInsightSlider();
+useTestimonialsSlider();
