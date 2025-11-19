cd7a3d4ad399278f11075ab16bd0e02ab17addfe
Des <dolboebi1@yandex.ru>
2025-11-20 00:35:09 +0700

    Created contact page

---
+++ src/js/contact.js
@@ -0,0 +1,9 @@
+import '/src/public/scss/contact.scss';
+
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { usePhone } from './components/contact/phone.js';
+
+useTheme();
+useBurger();
+usePhone();
