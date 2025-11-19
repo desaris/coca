24487e40c856444d4764ea3fe106060d14331bbb
Des <dolboebi1@yandex.ru>
2025-11-19 23:29:35 +0700

    Created pricing page

---
+++ src/js/pricing.js
@@ -0,0 +1,9 @@
+import '/src/public/scss/pricing.scss';
+
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { usePriceSwitcher } from './components/pricing/priceSwitcher.js';
+
+useTheme();
+useBurger();
+usePriceSwitcher();
