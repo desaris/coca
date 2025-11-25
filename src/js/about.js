cad477a201cc659674205dd500e354200639ebf1
Des <dolboebi1@yandex.ru>
2025-11-25 08:43:08 +0700

    Created about:articles section

--- src/js/about.js
+++ src/js/about.js
@@ -1,5 +1,9 @@
 import '/src/public/scss/about.scss';
 import { useHeroSlider } from './components/about/slider';
+import { useArticlesSlider } from './components/about/slider';
 import { useTheme } from './components/theme';
+import { useBurger } from './components/burger';
 useTheme();
+useBurger();
 useHeroSlider();
+useArticlesSlider();
--- src/js/components/about/slider.js
+++ src/js/components/about/slider.js
@@ -1,8 +1,40 @@
 import Swiper from 'swiper';
 import 'swiper/css';
+import { Navigation } from 'swiper/modules';
 
 export const useHeroSlider = () => {
     new Swiper('.hero__slider--first', {
+        slidesPerView: '3',
+        watchSlidesVisibility: false,
+        spaceBetween: 32,
+        loop: true,
+        centeredSlides: false,
+        breakpoints: {
+            320: {
+                slidesPerView: '1',
+                loop: true,
+            },
+
+            769: {
+                slidesPerView: '2',
+                loop: true,
+            },
+
+            1025: {
+                slidesPerView: 'auto',
+                loop: true,
+            },
+        },
+    });
+};
+
+export const useArticlesSlider = () => {
+    new Swiper('.articles__slider', {
+        modules: [Navigation],
+        navigation: {
+            nextEl: '.articles__btn--next',
+            prevEl: '.articles__btn--prev',
+        },
         slidesPerView: '3',
         watchSlidesVisibility: false,
         spaceBetween: 32,
